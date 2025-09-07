fd06c6a248c9c3fe6104702d178be2f69ecea940
Rohan Vishwakarma <rohanvishwakarma8261@gmail.com>
2025-09-07 15:35:08 +0530

    patient service and grpc done

---
+++ patient-service/src/main/java/com/slyder/patientservice/repository/PatientRepository.java
@@ -0,0 +1,13 @@
+package com.slyder.patientservice.repository;
+
+import java.util.UUID;
+
+import com.slyder.patientservice.model.Patient;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PatientRepository extends JpaRepository<Patient, UUID> {
+    boolean existsByEmail(String email);
+    boolean existsByEmailAndIdNot(String email, UUID id);
+}
